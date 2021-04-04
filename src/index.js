b76b83d64232c92b1871afb3aecfda5b26fe71ff
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-04-04 18:26:38 -0400

    BIMBPA

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+import ServiceWorker from '@crazy-corders/serviceWorker'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ServiceWorker forceInsatll={{styles:{{container: {color: 'black'}}} descrtipion:'adasda'}} />
   </React.StrictMode>,
   document.getElementById('root')
 )
